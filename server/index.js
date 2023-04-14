ee29844801f3778a157fb6865ca539db370693d4
Sergei Ivankov <sergeiivankov@pm.me>
2023-04-14 12:38:23 +0300

    Add security middleware

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import compression from 'compression';
 import * as dotenv from 'dotenv';
 import express from 'express';
+import helmet from 'helmet';
 import strictRoutes from './helpers/strict-routes.js';
 
 dotenv.config();
@@ -9,6 +10,7 @@ const app = express();
 app.enable('case sensitive routing');
 app.enable('strict routing');
 
+app.use(helmet());
 app.use(strictRoutes);
 app.use(compression());
 app.use(express.static('public', { redirect: false }));
