4a7d159fd2d0ce82d9bb6d1647f81c30bd37ab73
Sergei Ivankov <sergeiivankov@pm.me>
2023-04-14 12:21:30 +0300

    Fix strict routes

--- server/helpers/strict-routes.js
+++ server/helpers/strict-routes.js
@@ -4,12 +4,12 @@ export default (req, res, next) => {
   let needFixPath = false;
 
   if(req.path.toLowerCase() !== req.path) needFixPath = true;
-  if(req.path.endsWith('/')) needFixPath = true;
+  if(req.path !== '/' && req.path.endsWith('/')) needFixPath = true;
 
   if(needFixPath) {
-    const url = new URL(req.originalUrl);
+    const url = new URL(req.originalUrl, 'http://localhost');
     url.pathname = url.pathname.toLowerCase().replace(trailingSlashRegExp, '');
-    res.redirect(301, url.toString());
+    res.redirect(301, url.pathname + url.search);
   } else {
     next();
   }
